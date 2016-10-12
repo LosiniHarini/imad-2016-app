704e4985ee3f1faa91243b556ce616429c311ca8
Losini kruthika harini <losini4298@gmail.com>
2016-10-12 14:57:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ element.innerHTML='NEW VALUE';
 //move the image
 var img=document.getElementById('disney');
 img.onclick=function(){
-    img.style.marginleft='100px';
+    img.style.marginleft='200px';
 };
