012e4669bb002c36cce1f55880b176902a2d7d87
Losini kruthika harini <losini4298@gmail.com>
2016-11-25 08:58:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,36 +1,6 @@
 console.log('Loaded!');
 var element=document.getElementById('maintext');
 var c=0;
-var n1=document.getElementById('num1').value;
-var n2=document.getElementById('num2').value;
-
-var sum=document.getElementById('add');
-sum.onclick=function()
-{
-   var result=parseInt(n1)+parseInt(n2);
-    if (!isNaN(result)) {
-           document.getElementById('res').value = result;
-       }
-    
-};
-var subt=document.getElementById('sub');
-subt.onclick=function()
-{
-    re.Value=n1-n2;
-    
-};
-var mult=document.getElementById('mul');
-mult.onclick=function()
-{
-    re.Value=n1*n2;
-    
-};
-var div=document.getElementById('divi');
-div.onclick=function()
-{
-    re.Value=n1/n2;
-    
-};
 
 element.innerHTML='NEW VALUE';
 //move the image
